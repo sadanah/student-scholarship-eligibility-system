b96ea31fef7d1f66bc8f83f88ebbc3ae1625a520
Sadana Herath <sadana.herath@gmail.com>
2026-06-24 16:24:20 +0530

    more formatting

--- src/student/scholarship/eligibility/system/Main.java
+++ src/student/scholarship/eligibility/system/Main.java
@@ -15,13 +15,17 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.print("=================================================\n");
+        System.out.print("     STUDENT SCHORLARSHIP ELIGIBILITY SYSTEM\n");
+        System.out.print("=================================================\n\n");
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("How many students do you want to process? ");
         int stuCount = scanner.nextInt();
-        System.out.println("\n\n");
 
         while (stuCount > 0) {
+            System.out.println("\n");
+
             System.out.print("Enter student name: ");
             String studentName = scanner.next();
 
--- src/student/scholarship/eligibility/system/Student.java
+++ src/student/scholarship/eligibility/system/Student.java
@@ -63,7 +63,7 @@ public class Student {
     public void displayDetails() {
         System.out.println("\n\n");
         System.out.println("Student Details:");
-        System.out.println("\n");
+        System.out.println("");
         System.out.println("Student Name: " + studentName);
         System.out.println("Student ID: " + studentID);
         System.out.println("GPA: " + GPA);
