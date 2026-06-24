88b7e465f1370356effc07b606fd3c295b21e44b
Sadana Herath <sadana.herath@gmail.com>
2026-06-24 16:01:05 +0530

    calculate scholarship method

--- src/student/scholarship/eligibility/system/Student.java
+++ src/student/scholarship/eligibility/system/Student.java
@@ -15,12 +15,36 @@ public class Student {
     double GPA;
     char activityLevel;
     
+    int scholarshipAmount = 0;
+    String scholarshipCategory = "";
+    
     //Constructor
     public Student (String studentName, int studentID, double GPA, char activityLevel){
         this.studentName =  studentName;
         this.studentID = studentID;
         this.GPA = GPA;
         this.activityLevel = activityLevel;
+    }
+
+    public void calculateScholarship() {
+
+        if (GPA >= 3.5) {
+            if (activityLevel == 'A') {
+                scholarshipAmount = 100000;
+                scholarshipCategory = "Full Scholarship";
+            } 
+            else if (activityLevel == 'B') {
+                scholarshipAmount = 50000;
+                scholarshipCategory = "Half Scholarship";
+            } 
+            else if (activityLevel == 'C') {
+                scholarshipAmount = 25000;
+                scholarshipCategory = "Merit Scholarship";
+            }
+        } else {
+            scholarshipAmount = 0;
+            scholarshipCategory = "Not Eligible for Scholarship";
+        }
     }
 }
 
